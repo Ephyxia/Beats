c1ade6f9b990549b17110c5fad9771b72596e44c
Ephyxia <limitedxstudios@gmail.com>
2013-12-23 20:08:26 -0500

    Set up basic window frame

--- Beats/src/net/Ephyxia/Beats/Beats.java
+++ Beats/src/net/Ephyxia/Beats/Beats.java
@@ -1,8 +1,44 @@
 package net.Ephyxia.Beats;
 
-public class Beats {
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
-	public static void main(String[] args) {
+public class Beats extends BasicGame {
+	
+	public static final String TITLE = "Beats";
+	public static final int width = 1360;
+	public static final int height = 768;
 
+	public Beats() {
+		super(TITLE);
+	}
+	
+	@Override
+	public void init(GameContainer gc) throws SlickException {
+		
+	}
+
+	@Override
+	public void update(GameContainer gc, int delta) throws SlickException {
+
+	}
+	
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+		
+	}
+	
+	public static void main(String[] args) throws SlickException {
+		AppGameContainer app = new AppGameContainer(new Beats());
+		app.setDisplayMode(width, height, false);
+		app.setAlwaysRender(true);
+		app.setUpdateOnlyWhenVisible(false);
+		app.setTargetFrameRate(120);
+		app.setShowFPS(false);
+		
+		app.start();
 	}
 }
