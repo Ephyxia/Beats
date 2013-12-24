38b6abb38bdf3fa9e3811b2f98810f8d1021c47f
Ephyxia <limitedxstudios@gmail.com>
2013-12-24 15:27:31 -0500

    Animated the menu buttons so the slide over when the mouse is hovered

--- Beats/src/net/Ephyxia/Beats/GUI/GUIMenuButton.java
+++ Beats/src/net/Ephyxia/Beats/GUI/GUIMenuButton.java
@@ -15,6 +15,8 @@ public class GUIMenuButton extends GUIButton {
 
 	private boolean hovered = false;
 	private boolean mouseDown = false;
+	
+	private float xOffset = 0.0f;
 
 	public GUIMenuButton(float x, float y, float width, float height, String text) {
 		this.x = x;
@@ -28,6 +30,21 @@ public class GUIMenuButton extends GUIButton {
 	void update(Input in, int delta) {
 		resetFlags();
 		updateState(in, delta);
+		
+		if(hovered) {
+			if(xOffset > -40f) {
+				xOffset -= .3f * delta;
+			}
+		}else {
+			if(xOffset < 0) {
+				xOffset += .3f * delta;
+			}
+		}
+		
+		if(xOffset > 0)
+			xOffset = 0;
+		if(xOffset < -40f)
+			xOffset = -40f;
 
 	}
 
@@ -54,8 +71,8 @@ public class GUIMenuButton extends GUIButton {
 		Image bLeft = Skin.buttonLeft.getScaledCopy(height / Skin.buttonLeft.getHeight());
 		Image bMiddle= Skin.buttonMiddle.getScaledCopy((int) width, (int)(height/Skin.buttonMiddle.getHeight() * Skin.buttonMiddle.getHeight()));
 		
-		bMiddle.draw(x + bLeft.getWidth(), y);
-		bLeft.draw(x, y);
+		bMiddle.draw(x + bLeft.getWidth() + xOffset, y);
+		bLeft.draw(x + xOffset, y);
 
 		pushColor(g);
 	}
@@ -68,8 +85,6 @@ public class GUIMenuButton extends GUIButton {
 	@Override
 	public void onHover(Input in, int delta) {
 		hovered = true;
-		
-		
 	}
 
 	@Override
